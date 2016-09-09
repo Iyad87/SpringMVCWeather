4628f7eb90beb3d9e7d30bbf97d92d36431588dd
Tim Janusz <TJanusz@comcast.net>
2016-09-09 15:13:09 -0400

    Adding spring MVC controller test using MockMvc

--- src/com/weather/controller/WeatherController.java
+++ src/com/weather/controller/WeatherController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -42,59 +43,34 @@ public class WeatherController {
 		this.forecastRetriever = forecastRetriever;
 	}
 
-	@RequestMapping(value = "/forecast1", method= RequestMethod.GET)
-	public CurrentForecast getForecast1() {
-		CurrentForecast forecast = new CurrentForecast();
-		return forecast;
-	}
+	/** This is example of how to have same endpoint with two different view resolvers (JSON,JSP) **/
+//	@RequestMapping(value = "/forecast2", method=RequestMethod.GET, produces="application/json")
+//	public ForecastResponse getForecast2(HttpServletRequest httpRequest) {
+//		// hard code Newtown square for now ("39.9869", "-75.4007")
+//		MapAPIResponse mapResponse = this.getMapInfoRetriever().getMapInfoFor("Newtown Square", "PA");
+//		Location loc = mapResponse.getGeometry().getLocation();
+//		System.out.println("Lat is: " + loc.getLatitude() + " Long is: " + loc.getLongitude() + " " 
+//				+ mapResponse.getPlaceId() + " " + mapResponse.getFormattedAddress());
+//		ForecastResponse forecastResponse = forecastRetriever.getForcastFor(String.valueOf(loc.getLatitude()),
+//				String.valueOf(loc.getLongitude()));
+//	    return forecastResponse;
+//	}
+//
+//	@RequestMapping(value = "/forecast2", method=RequestMethod.GET)
+//	public ModelAndView getForecast2HTML(HttpServletRequest httpRequest) {
+//		// Should probably use content negotiation strategy here but for now just add HTML response
+//		ForecastResponse forecastResponse = getForecast2(httpRequest);
+//		ModelAndView modelAndView = new ModelAndView("forecast1");
+//		modelAndView.addObject("forecast", forecastResponse.getCurrently());
+//		modelAndView.addObject("message", "hello from HTML world");
+//		return modelAndView;
+//	}
 
-	@RequestMapping(value = "/forecast2", method= RequestMethod.GET)
-	public CurrentForecast getForecast2() {
-		CurrentForecast forecast = new CurrentForecast();
-		forecast.setSummary("test summary");
-		forecast.setTemperature(86.6d);
-		return forecast;
-	}
-
-	@RequestMapping(value = "/forecast3/{forecastId}", method=RequestMethod.GET)
-	public CurrentForecast getForecast3(@PathVariable("forecastId") long forecastId) {
-		CurrentForecast forecast = new CurrentForecast();
-		forecast.setSummary("test summary");
-		forecast.setTemperature(86.6d);
-		forecast.setDewPoint(forecastId);
-		return forecast;
-	}
-
-	@RequestMapping(value = "/forecast6", method=RequestMethod.GET, produces="application/json")
-	public ForecastResponse getForecast6(HttpServletRequest httpRequest) {
-		// hard code Newtown square for now ("39.9869", "-75.4007")
-		MapAPIResponse mapResponse = this.getMapInfoRetriever().getMapInfoFor("Newtown Square", "PA");
-		Location loc = mapResponse.getGeometry().getLocation();
-		System.out.println("Lat is: " + loc.getLatitude() + " Long is: " + loc.getLongitude() + " " 
-				+ mapResponse.getPlaceId() + " " + mapResponse.getFormattedAddress());
-		ForecastResponse forecastResponse = forecastRetriever.getForcastFor(String.valueOf(loc.getLatitude()),
-				String.valueOf(loc.getLongitude()));
-	    return forecastResponse;
-	}
-
-	@RequestMapping(value = "/forecast6", method=RequestMethod.GET)
-	public ModelAndView getForecast6HTML(HttpServletRequest httpRequest) {
-		// Should probably use content negotiation strategy here but for now just add HTML response
-		ForecastResponse forecastResponse = getForecast6(httpRequest);
-		ModelAndView modelAndView = new ModelAndView("forecast1");
-		modelAndView.addObject("forecast", forecastResponse.getCurrently());
-		modelAndView.addObject("message", "hello from HTML world");
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/forecast7/{city},{state}", method=RequestMethod.GET, produces="application/json")
-	public ForecastResponse getForecast7(@PathVariable("city") String city,
+	@RequestMapping(value = "/forecast/{city},{state}", method=RequestMethod.GET, produces="application/json")
+	public ForecastResponse getForecast(@PathVariable("city") String city,
 			@PathVariable("state") String state) {
 		MapAPIResponse mapResponse = this.getMapInfoRetriever().getMapInfoFor(city, state);
 		Location loc = mapResponse.getGeometry().getLocation();
-		System.out.println("Lat is: " + loc.getLatitude() + " Long is: " + loc.getLongitude() + " " 
-				+ mapResponse.getPlaceId() + " " + mapResponse.getFormattedAddress());
-//		ForecastResponse forecastResponse = forecastRetriever.getForcastFor("hello","world");
 		ForecastResponse forecastResponse = forecastRetriever.getForcastFor(String.valueOf(loc.getLatitude()),
 				String.valueOf(loc.getLongitude()));
 		return forecastResponse;
---
+++ src/com/weather/controller/WeatherControllerIntegrationTest.java
@@ -0,0 +1,126 @@
+package com.weather.controller;
+
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.weather.model.ForecastResponse;
+import com.weather.model.Geometry;
+import com.weather.model.Location;
+import com.weather.model.MapAPIResponse;
+import com.weather.model.exceptions.ExternalServiceInvocationException;
+import com.weather.service.ForecastRetriever;
+import com.weather.service.MapInfoRetriever;
+
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:/weatherControllerTest-servlet.xml"})
+@PropertySource("classpath:/app-test.properties")
+public class WeatherControllerIntegrationTest extends AbstractJUnit4SpringContextTests {
+
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ForecastRetriever forecastRetriever;
+	
+	@Autowired
+	private MapInfoRetriever mapInfoRetriever;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    
+	@Before
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		Mockito.reset(forecastRetriever);
+		Mockito.reset(mapInfoRetriever);
+	}
+	
+	private MediaType getMediaType() {
+		return MediaType.APPLICATION_JSON;
+	}
+	
+	@Test
+	public void testGetForecastReturnsHTTP_OK_OnValidRequest() throws Exception {
+		// mock service calls
+		Mockito.when(mapInfoRetriever.getMapInfoFor("TestCity", "PA")).thenReturn(buildStubMapResponse());
+		Mockito.when(forecastRetriever.getForcastFor("100.0", "100.0")).thenReturn(buildStubForecastResponse());
+
+		// make GET request to endpoint
+		mockMvc.perform(MockMvcRequestBuilders.get("/weather/forecast/TestCity,PA").accept(getMediaType()).contentType(getMediaType()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.longitude", Matchers.is(100)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.latitude", Matchers.is(100)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.offset", Matchers.is(200)));
+//                .andDo(MockMvcResultHandlers.print());
+		
+        Mockito.verify(mapInfoRetriever, Mockito.times(1)).getMapInfoFor("TestCity", "PA");
+        Mockito.verifyNoMoreInteractions(mapInfoRetriever);
+        Mockito.verify(forecastRetriever, Mockito.times(1)).getForcastFor("100.0", "100.0");
+        Mockito.verifyNoMoreInteractions(forecastRetriever);
+	}
+
+	@Test
+	public void testGetForecastReturnsBAD_GATEWAY_OnGoogleServiceInvocationError() throws Exception {
+		// mock service calls
+		ExternalServiceInvocationException ex = new ExternalServiceInvocationException("Google", "failed");
+		Mockito.when(mapInfoRetriever.getMapInfoFor("TestCity", "PA")).thenThrow(ex);
+
+		// make GET request to endpoint
+		mockMvc.perform(MockMvcRequestBuilders.get("/weather/forecast/TestCity,PA").accept(getMediaType()).contentType(getMediaType()))
+                .andExpect(MockMvcResultMatchers.status().isBadGateway());
+		
+        Mockito.verify(mapInfoRetriever, Mockito.times(1)).getMapInfoFor("TestCity", "PA");
+        Mockito.verifyNoMoreInteractions(mapInfoRetriever);
+	}
+
+	@Test
+	public void testGetForecastReturnsBAD_GATEWAY_OnForecastIOServiceInvocationError() throws Exception {
+		// mock service calls
+		Mockito.when(mapInfoRetriever.getMapInfoFor("TestCity", "PA")).thenReturn(buildStubMapResponse());
+		ExternalServiceInvocationException ex = new ExternalServiceInvocationException("FORECAST", "failed");
+		Mockito.when(forecastRetriever.getForcastFor("100.0", "100.0")).thenThrow(ex);
+
+		// make GET request to endpoint
+		mockMvc.perform(MockMvcRequestBuilders.get("/weather/forecast/TestCity,PA").accept(getMediaType()).contentType(getMediaType()))
+                .andExpect(MockMvcResultMatchers.status().isBadGateway());
+		
+        Mockito.verify(mapInfoRetriever, Mockito.times(1)).getMapInfoFor("TestCity", "PA");
+        Mockito.verifyNoMoreInteractions(mapInfoRetriever);
+        Mockito.verify(forecastRetriever, Mockito.times(1)).getForcastFor("100.0", "100.0");
+        Mockito.verifyNoMoreInteractions(forecastRetriever);
+	}
+	
+	private ForecastResponse buildStubForecastResponse() {
+		ForecastResponse stubForecastResponse = new ForecastResponse();
+		stubForecastResponse.setLatitude(100);
+		stubForecastResponse.setLongitude(100);
+		stubForecastResponse.setOffset(200);
+		return stubForecastResponse;
+	}
+	
+	private MapAPIResponse buildStubMapResponse() {
+		MapAPIResponse stubMapResponse = new MapAPIResponse();
+		Geometry stubGeo = new Geometry();
+		Location stubLocation = new Location();
+		stubLocation.setLatitude(100);
+		stubLocation.setLongitude(100);
+		stubGeo.setLocation(stubLocation);
+		stubMapResponse.setGeometry(stubGeo);
+		return stubMapResponse;
+	}
+
+}
